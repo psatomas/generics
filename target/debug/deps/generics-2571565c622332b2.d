77c0d2cce19ff0c45d1e0f3c1432fad7a3b5fd4c
psatomas <psatomas@gmail.com>
2025-07-06 18:55:33 -0300

    first commit

---
+++ target/debug/deps/generics-2571565c622332b2.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/generics/target/debug/deps/libgenerics-2571565c622332b2.rmeta: src/main.rs
+
+/home/tomas/Documents/projects/rust/generics/target/debug/deps/generics-2571565c622332b2.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/generics-e8b3750c6647e153.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/generics/target/debug/deps/libgenerics-e8b3750c6647e153.rmeta: src/main.rs
+
+/home/tomas/Documents/projects/rust/generics/target/debug/deps/generics-e8b3750c6647e153.d: src/main.rs
+
+src/main.rs:
