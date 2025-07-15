995ea6e7afb63aa260d6a6be9234514c85c8c894
psatomas <psatomas@gmail.com>
2025-07-15 17:14:45 -0300

    feat: added coding_challenge.rs

--- src/bin/coding_challenge.rs
+++ src/bin/coding_challenge.rs
@@ -5,12 +5,40 @@ enum DigitalContent {
     VideoFile,
 }
 
+#[derive(Debug)]
 struct ChatMessage<T> {
     content: T,
     time: String,
 } 
 
+impl ChatMessage<DigitalContent> {
+    fn consume_entertaiment(&self) {
+        println!("watching the {:?}", self.content)
+    }
+}
 
+impl<T> ChatMessage<T>  {
+    fn retrieve_time(&self) -> String {
+        self.time.clone()
+    }    
+}
 fn main() {
+    let message = ChatMessage {
+        content: "Hi, lol",
+        time: String::from("2025-07-15"),
+    };
+    println!("{}", message.retrieve_time());
     
+    let notification = ChatMessage {
+        content: "what's your favorite pizza topping?",
+        time: String::from("2025-07-16"),
+    };
+    println!("{}", notification.retrieve_time());
+    
+    let audio = ChatMessage {
+        content: DigitalContent::AudioFile,
+        time: String::from("2025-07-17"),
+    };
+    audio.consume_entertaiment();
+    println!("{}", audio.retrieve_time());
 }
